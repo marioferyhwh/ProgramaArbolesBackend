71c81944f10baa976caf7330970a97ae55507c2e
mario robayo <marioferyhwh@gmail.com>
2020-05-30 14:20:22 -0500

    se realiza la primera prueba a la direcion login y funciona perfectamente

--- main.go
+++ main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/marioferyhwh/IMFBackend_forest/commons"
 	"github.com/marioferyhwh/IMFBackend_forest/migration"
-	"github.com/marioferyhwh/IMFBackend_forest/models"
+	"github.com/marioferyhwh/IMFBackend_forest/routes"
+	"github.com/urfave/negroni"
 )
 
 func main() {
-	fmt.Println("Inicia Backend")
+	log.Println("Inicia Backend")
 	var migrate string
 	flag.StringVar(&migrate, "migrate", "no", "Generar la migracion")
 	flag.IntVar(&commons.Port, "port", 8080, " puerto del servidor")
@@ -18,8 +21,19 @@ func main() {
 	if migrate == "yes" {
 		migration.Migrate()
 	}
-	var user models.User
 
-	fmt.Println(user)
-	fmt.Println("Finaliza Backend")
+	router := routes.InitRoutes()
+	//middleware
+	n := negroni.Classic()
+	n.UseHandler(router)
+
+	//inicio del servidor
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", commons.Port),
+		Handler: n,
+	}
+	log.Println("Inicia servidor en el puerto:", commons.Port)
+	log.Panicln(server.ListenAndServe())
+	log.Println("servicion detenido ¿eso es lo que queria?")
+	// fmt.Println("Finaliza Backend")
 }
