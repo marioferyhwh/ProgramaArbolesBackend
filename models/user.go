0ea3ce20fc76b17f2871fc5d9d319e0429968809
mario robayo <marioferyhwh@gmail.com>
2020-06-20 10:59:06 -0500

    se agrega fecha de cambio de clave y bollano para obligar cambio de clave

--- models/user.go
+++ models/user.go
@@ -1,18 +1,22 @@
 package models
 
+import "time"
+
 //User usuario
 type User struct {
 	Model
-	Actived         bool   `json:"actived,omitempty"          gorm:"type:bool;not null;default:true"`
-	NickName        string `json:"nick_name,omitempty"        gorm:"type:varchar(50);not null;default:''"`
-	Email           string `json:"email,omitempty"            gorm:"type:varchar(100);not null"`
-	Password        string `json:"password,omitempty"         gorm:"type:varchar(256);not null"`
-	CodDocumentType string `json:"document_code,omitempty"    gorm:"type:varchar(3);not null;default:''"`
-	Document        string `json:"document,omitempty"         gorm:"type:numeric(11);not null"`
-	Name            string `json:"name,omitempty"             gorm:"type:varchar(50);not null;default:''"`
-	Admin           bool   `json:"admin,omitempty"            gorm:"type:bool;not null;default:false"`
-	TimeZone        int8   `json:"time_zone,omitempty"        gorm:"type:smallint;not null;default:-5"`
-	ConfirmPassword string `json:"confirm_password,omitempty" gorm:"-"`
+	Actived         bool       `json:"actived,omitempty"          gorm:"type:bool;not null;default:true"`
+	NickName        string     `json:"nick_name,omitempty"        gorm:"type:varchar(50);not null;default:''"`
+	Email           string     `json:"email,omitempty"            gorm:"type:varchar(100);not null"`
+	Password        string     `json:"password,omitempty"         gorm:"type:varchar(256);not null"`
+	CodDocumentType string     `json:"document_code,omitempty"    gorm:"type:varchar(3);not null;default:''"`
+	Document        string     `json:"document,omitempty"         gorm:"type:numeric(11);not null"`
+	Name            string     `json:"name,omitempty"             gorm:"type:varchar(50);not null;default:''"`
+	Admin           bool       `json:"admin,omitempty"            gorm:"type:bool;not null;default:false"`
+	TimeZone        int8       `json:"time_zone,omitempty"        gorm:"type:smallint;not null;default:-5"`
+	PassewordAt     *time.Time `json:"password_at,omitempty"      gorm:"NOT NULL;DEFAULT:CURRENT_TIMESTAMP"`
+	ResetPassword   bool       `json:"change-password,omitempty"  gorm:"type:bool;not null;default:true"`
+	ConfirmPassword string     `json:"confirm_password,omitempty" gorm:"-"`
 
 	UserCollection []UserCollection `json:"collections,omitempty"          gorm:"foreignkey:CodUser;association_foreignkey:id"`
 	UserTel        []UserTel        `json:"tels,omitempty"                 gorm:"foreignkey:CodUser;association_foreignkey:id"`
